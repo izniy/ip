f487f85163a7731c6879c7a2eddb1cc3fec41aac
izniy <ngyinzi1303@gmail.com>
2025-01-23 17:40:52 +0800

    Level-1: Add echo feature

--- src/main/java/bearbot/BearBot.java
+++ src/main/java/bearbot/BearBot.java
@@ -1,14 +1,28 @@
 package bearbot;
 
+import java.util.Scanner;
+
 public class BearBot {
+    // This class handles the program logic
     private Ui ui;
+    private Scanner scanner;
 
     public BearBot() {
         ui = new Ui();
+        scanner = new Scanner(System.in);
     }
 
     public void run() {
         ui.showWelcomeMessage();
+
+        String input;
+        while (true) { // infinite loop that will run till break
+            input = scanner.nextLine();
+            if (input.equalsIgnoreCase("bye")) {
+                break;
+            }
+            ui.echo(input);
+        }
         ui.showGoodbyeMessage();
     }
     public static void main(String[] args) {
--- src/main/java/bearbot/Ui.java
+++ src/main/java/bearbot/Ui.java
@@ -3,11 +3,15 @@ package bearbot;
 public class Ui {
     // User Interface class to handle interactions with user
     public void showWelcomeMessage() {
-        System.out.println(" Hello! I'm bearbot.BearBot");
+        System.out.println(" Hello! I'm BearBot");
         System.out.println(" What can I do for you?");
     }
 
     public void showGoodbyeMessage() {
         System.out.println(" Bye. Hope to see you again soon!");
+    }
+
+    public void echo(String input) {
+        System.out.println(input);
     }
 }
