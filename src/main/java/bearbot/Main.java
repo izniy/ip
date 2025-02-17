aec76649c8afa2b01a1901b2b932daf61cf4b4d1
izniy <ngyinzi1303@gmail.com>
2025-02-18 01:20:57 +0800

    Add application name

--- src/main/java/bearbot/Main.java
+++ src/main/java/bearbot/Main.java
@@ -23,6 +23,7 @@ public class Main extends Application {
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
+            stage.setTitle("Bearbot.exe");
             fxmlLoader.<MainWindow>getController().setBearBot(bearBot);
             stage.show();
         } catch (IOException e) {
